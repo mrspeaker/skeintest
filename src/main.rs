907a2a8b98a85cac24f8a5aa085bf7e107500519
Mr Speaker <mrspeaker@gmail.com>
2025-04-05 21:18:01 +0800

    oh depth

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use bevy::{
     prelude::*,
     scene::SceneInstanceReady,
-    core_pipeline::{
-        bloom::Bloom,
-        prepass::{ DepthPrepass, NormalPrepass }, dof::DepthOfField, tonemapping::Tonemapping
-    }
+    core_pipeline::prepass::{ DepthPrepass, MotionVectorPrepass, NormalPrepass },
+    render::{
+        render_resource::{AsBindGroup, ShaderRef, ShaderType},
+        camera::ScalingMode
+    },
 };
 use bevy_asset_loader::prelude::*;
 use bevy_asset_loader::asset_collection::AssetCollection;
@@ -13,6 +14,22 @@ use std::f32::consts::*;
 use avian3d::prelude::*;
 
 use std::f32::consts::TAU;
+
+const PREPASS_SHADER_ASSET_PATH: &str = "shaders/show_prepass.wgsl";
+const MATERIAL_SHADER_ASSET_PATH: &str = "shaders/custom_material.wgsl";
+
+static mut VIEW:bool = false;
+
+#[derive(Resource)]
+struct GoalsReached {
+    main_goal: bool,
+    bonus: u32,
+}
+
+#[derive(Resource)]
+struct Config {
+    view: bool,
+}
 
 #[derive(Component)]
 struct Playa;
@@ -78,6 +95,62 @@ struct AnimationsToPlay {
     indices: Vec<AnimationNodeIndex>,
 }
 
+// This is the struct that will be passed to your shader
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct CustomMaterial {
+    #[uniform(0)]
+    color: LinearRgba,
+    #[texture(1)]
+    #[sampler(2)]
+    color_texture: Option<Handle<Image>>,
+    alpha_mode: AlphaMode,
+}
+
+/// Not shown in this example, but if you need to specialize your material, the specialize
+/// function will also be used by the prepass
+impl Material for CustomMaterial {
+    fn fragment_shader() -> ShaderRef {
+        MATERIAL_SHADER_ASSET_PATH.into()
+    }
+
+    fn alpha_mode(&self) -> AlphaMode {
+        self.alpha_mode
+    }
+
+    // You can override the default shaders used in the prepass if your material does
+    // anything not supported by the default prepass
+    // fn prepass_fragment_shader() -> ShaderRef {
+    //     "shaders/custom_material.wgsl".into()
+    // }
+}
+
+#[derive(Debug, Clone, Default, ShaderType)]
+struct ShowPrepassSettings {
+    show_depth: u32,
+    show_normals: u32,
+    show_motion_vectors: u32,
+    padding_1: u32,
+    padding_2: u32,
+}
+
+// This shader loads the prepass texture and outputs it directly
+#[derive(Asset, TypePath, AsBindGroup, Debug, Clone)]
+struct PrepassOutputMaterial {
+    #[uniform(0)]
+    settings: ShowPrepassSettings,
+}
+
+impl Material for PrepassOutputMaterial {
+    fn fragment_shader() -> ShaderRef {
+        PREPASS_SHADER_ASSET_PATH.into()
+    }
+
+    // This needs to be transparent in order to show the scene behind the mesh
+    fn alpha_mode(&self) -> AlphaMode {
+        AlphaMode::Blend
+    }
+}
+
 fn main() {
     App::new()
         .register_type::<Player>()
@@ -89,7 +162,14 @@ fn main() {
             DefaultPlugins.set(ImagePlugin::default_nearest()),
             // PhysicsDebugPlugin::default(),
             PhysicsPlugins::default(),
-            SkeinPlugin::default()
+            SkeinPlugin::default(),
+            MaterialPlugin::<CustomMaterial>::default(),
+            MaterialPlugin::<PrepassOutputMaterial> {
+                // This material only needs to read the prepass textures,
+                // but the meshes using it should not contribute to the prepass render, so we can disable it.
+                prepass_enabled: false,
+                ..default()
+            },
         ))
         .init_state::<GameStates>()
         .add_loading_state(
@@ -102,8 +182,9 @@ fn main() {
             file_drop,
             update_cam,
             update_spin,
-            update_playa
-        ))
+            update_playa,
+            toggle_prepass_view
+        ).run_if(in_state(GameStates::Next)))
         .add_observer(on_dropped)
         .run();
 }
@@ -113,7 +194,10 @@ fn setup(
     asset_server: Res<AssetServer>,
     player: Res<PlayerAssets>,
     mut graphs: ResMut<Assets<AnimationGraph>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut depth_materials: ResMut<Assets<PrepassOutputMaterial>>,
 ) {
+    commands.insert_resource(Config { view: false });
     commands.spawn((
         MyCam,
         Camera3d::default(),
@@ -121,24 +205,36 @@ fn setup(
             hdr: true,
             ..default()
         },
-        Tonemapping::TonyMcMapface,
-        Bloom::NATURAL,
-        //DepthPrepass,
-        //NormalPrepass,
+        Projection::from(OrthographicProjection {
+            scaling_mode: ScalingMode::FixedVertical {
+                viewport_height: 10.0, // world units per pixel of window height.
+            },
+            ..OrthographicProjection::default_3d()
+        }),
         //Bloom::default(),
-        DepthOfField {
-            focal_distance: 499.0,
-            aperture_f_stops: 20.0,
-            ..default()
-        },
-        Transform::from_xyz(2.0, 5.0, 7.0)
-            .looking_at(Vec3::new(-2.0, 2.0, 0.0), Dir3::Y),
+        DepthPrepass,
+        NormalPrepass,
+        MotionVectorPrepass,
+//        Transform::from_xyz(2.0, 5.0, 7.0)
+//            .looking_at(Vec3::new(-2.0, 2.0, 0.0), Dir3::Y),
+        Transform::from_xyz(7.0, 5.0, 7.0)
+            .looking_at(Vec3::new(0.0, 3.0, 0.0), Dir3::Y),
         EnvironmentMapLight {
             diffuse_map: asset_server.load("hdrs/pisa_diffuse_rgb9e5_zstd.ktx2"),
             specular_map: asset_server.load("hdrs/pisa_specular_rgb9e5_zstd.ktx2"),
             intensity: 700.0,
             ..default()
         },
+    ));
+
+    commands.spawn((
+        Mesh3d(meshes.add(Rectangle::new(5.0, 5.0))),
+        MeshMaterial3d(depth_materials.add(PrepassOutputMaterial {
+            settings: ShowPrepassSettings::default(),
+        })),
+        Transform::from_xyz(-3.0, 2.0, 0.0)
+            //.looking_at(Vec3::new(0.0, 0.0, 0.0), Vec3::Y),
+    //    NotShadowCaster,
     ));
 
     commands.spawn((
@@ -267,11 +363,23 @@ fn on_dropped(
 
 fn update_cam(
     mut cam: Query<&mut Transform, With<MyCam>>,
+    keycode: Res<ButtonInput<KeyCode>>,
+    mut config: ResMut<Config>,
     time: Res<Time>
 ) {
     let secs = time.elapsed_secs_wrapped();
     for mut t in cam.iter_mut() {
         t.translation.y += (secs * 0.5).sin() * 0.0025;
+        if keycode.just_pressed(KeyCode::Digit1) {
+            config.view = false;
+            *t = Transform::from_xyz(-7.0, 1.0, 7.0)
+                .looking_at(Vec3::new(-7.0, 1.0, 0.0), Dir3::Y);
+        }
+        if keycode.just_pressed(KeyCode::Digit2) {
+            config.view = true;
+            *t = Transform::from_xyz(7.0, 1.0, -6.5)
+                .looking_at(Vec3::new(0.0, 1.0, -6.5), Dir3::Y);
+        }
     }
 }
 
@@ -325,6 +433,7 @@ fn update_playa(
     mut player: Query<(Entity, &mut Transform), With<Playa>>,
     time: Res<Time>,
     input: Res<ButtonInput<KeyCode>>,
+    config: Res<Config>,
     animations_to_play: Query<&AnimationsToPlay>,
     mut players: Query<&mut AnimationPlayer, With<PlayerPlayer>>,
 ) {
@@ -342,21 +451,40 @@ fn update_playa(
         let power = 2.0;
         let anim_speed = 1.5;
         let mut v = Vec2::new(0.0, 0.0);
-        if input.pressed(KeyCode::KeyW) {
-            v.y -= power;
-            t.rotation = Quat::from_rotation_y(-PI);
-        }
-        if input.pressed(KeyCode::KeyS) {
-            v.y += power;
-            t.rotation = Quat::from_rotation_y(0.0);
-        }
-        if input.pressed(KeyCode::KeyA) {
-            v.x -= power;
-            t.rotation = Quat::from_rotation_y(-PI / 2.0);
-        }
-        if input.pressed(KeyCode::KeyD) {
-            v.x += power;
-            t.rotation = Quat::from_rotation_y(PI / 2.0);
+        if config.view {
+            if input.pressed(KeyCode::KeyW) {
+                v.x -= power;
+                t.rotation = Quat::from_rotation_y(-PI * 0.5);
+            }
+            if input.pressed(KeyCode::KeyS) {
+                v.x += power;
+                t.rotation = Quat::from_rotation_y(PI * 0.5);
+            }
+            if input.pressed(KeyCode::KeyA) {
+                v.y += power;
+                t.rotation = Quat::from_rotation_y(0.0);
+            }
+            if input.pressed(KeyCode::KeyD) {
+                v.y -= power;
+                t.rotation = Quat::from_rotation_y(PI);
+            }
+        } else {
+            if input.pressed(KeyCode::KeyW) {
+                v.y -= power;
+                t.rotation = Quat::from_rotation_y(-PI);
+            }
+            if input.pressed(KeyCode::KeyS) {
+                v.y += power;
+                t.rotation = Quat::from_rotation_y(0.0);
+            }
+            if input.pressed(KeyCode::KeyA) {
+                v.x -= power;
+                t.rotation = Quat::from_rotation_y( -PI / 2.0);
+            }
+            if input.pressed(KeyCode::KeyD) {
+                v.x += power;
+                t.rotation = Quat::from_rotation_y(PI / 2.0);
+            }
         }
         if v.length() == 0.0 {
             anim_player.stop(animations.indices[0]);
@@ -367,6 +495,27 @@ fn update_playa(
         }
 
         t.translation.x += v.x * time.delta_secs();
+        if t.translation.x > 1.0 {
+            t.translation.x = 1.0;
+        }
         t.translation.z += v.y * time.delta_secs();
+        if t.translation.z > 1.0 {
+            t.translation.z = 1.0;
+        }
+    }
+}
+
+fn toggle_prepass_view(
+    mut prepass_view: Local<u32>,
+    keycode: Res<ButtonInput<KeyCode>>,
+    material_handle: Single<&MeshMaterial3d<PrepassOutputMaterial>>,
+    mut materials: ResMut<Assets<PrepassOutputMaterial>>,
+) {
+    if keycode.just_pressed(KeyCode::Space) {
+        *prepass_view = (*prepass_view + 1) % 4;
+        let mat = materials.get_mut(*material_handle).unwrap();
+        mat.settings.show_depth = (*prepass_view == 1) as u32;
+        mat.settings.show_normals = (*prepass_view == 2) as u32;
+        mat.settings.show_motion_vectors = (*prepass_view == 3) as u32;
     }
 }
